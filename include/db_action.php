6e0ac08d67dcef348a04ca8d8337a206d627509d
Dasha Chizhova <103762177+DashaChizhova@users.noreply.github.com>
2024-06-06 19:41:23 +0300

    .

--- include/db_action.php
+++ include/db_action.php
@@ -1,7 +1,7 @@
 <?php
-defined('s@>J$qw$i8_5rvY=6d{Z@!,V%J[J4Z^8C3q*bO$%/_db~iy6Fz=eTL/^O-@VKJU{E=U^x,JfooR19xKpgQ*,A/Dbg+9@>J1%.T[sL9#-4!-A8]t') or die('Доступ запрещён!');
+// defined('s@>J$qw$i8_5rvY=6d{Z@!,V%J[J4Z^8C3q*bO$%/_db~iy6Fz=eTL/^O-@VKJU{E=U^x,JfooR19xKpgQ*,A/Dbg+9@>J1%.T[sL9#-4!-A8]t') or die('Доступ запрещён!');
 
-require_once __DIR__ . '/config.php';
+// require_once __DIR__ . '/config.php';
 
 function log_to_file_db($txt, $type = 'ERROR')
 {
@@ -18,14 +18,16 @@ $opt_p = [
   PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
   PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
   PDO::ATTR_EMULATE_PREPARES => false,
-//  PDO::MYSQL_ATTR_FOUND_ROWS => true
+  PDO::MYSQL_ATTR_FOUND_ROWS => true
 ];
 
 try {
-  $conn = new PDO('mysql:host=' . BD_HOST . ';dbname=' . BD_NAME . ';charset=utf8mb4', BD_USER, BD_PASS, $opt_p); //utf8mb4
+  $conn = new PDO('mysql:host=' . "localhost" . ';dbname=' . "project" . ';charset=utf8mb4', "root", "", $opt_p); //utf8mb4
+
 } catch (PDOException $e) {
   log_to_file_db('Подключение не удалось: ' . $e->getMessage());
   die('Подключение не удалось: ' . $e->getMessage());
+ 
 }
 
 /*
